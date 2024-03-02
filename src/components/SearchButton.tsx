439cf39ef66a376b7e67a8cf211cf7eb5ec8e3a9
ckwong <ckwong@esrisingapore.com.sg>
2024-03-02 21:33:34 +0800

    Implemented SearchDialog

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,7 +1,9 @@
 import { Search } from "lucide-react";
-import { Button } from "./ui/button";
 import { useEffect, useRef } from "react";
+
 import { view } from "@/arcgis";
+import { SearchDialog } from "./SearchDialog";
+import { Button } from "./ui/button";
 
 const SearchButton = () => {
   const ref = useRef<HTMLButtonElement>(null);
@@ -13,11 +15,13 @@ const SearchButton = () => {
   }, []);
 
   return (
-    <Button variant="outline" ref={ref} className="flex gap-4">
-      <Search />
-      Search ...
-      <kbd className="px-2 rounded-md bg-neutral-200">⌘ K</kbd>
-    </Button>
+    <SearchDialog>
+      <Button variant="outline" ref={ref} className="flex gap-4">
+        <Search />
+        Search ...
+        <kbd className="px-2 rounded-md bg-neutral-200">⌘ K</kbd>
+      </Button>
+    </SearchDialog>
   );
 };
 
---
+++ src/components/SearchDialog.tsx
@@ -0,0 +1,50 @@
+import { Search, X } from "lucide-react";
+import { useState } from "react";
+
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+
+interface SearchDialogProps {
+  children: React.ReactNode;
+}
+
+export const SearchDialog = ({ children }: SearchDialogProps) => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleClear = () => {
+    setSearch("");
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="p-0">
+        <DialogHeader>
+          <div className="flex items-center px-2">
+            <Search />
+            <Input
+              className="border-0 "
+              value={search}
+              onChange={handleChange}
+              placeholder="Search..."
+            />
+            <Button variant="ghost" size="icon" onClick={handleClear}>
+              <X />
+            </Button>
+          </div>
+        </DialogHeader>
+        <div></div>
+      </DialogContent>
+    </Dialog>
+  );
+};
