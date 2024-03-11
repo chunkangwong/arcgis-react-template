db2c5504f752a1677408152e8d1b35fe75e626c3
ckwong <ckwong@esrisingapore.com.sg>
2024-03-11 23:54:01 +0800

    Removed call of getObjectId

--- src/hooks/useSearchViewModel.ts
+++ src/hooks/useSearchViewModel.ts
@@ -8,12 +8,12 @@ export const useSearchViewModel = (searchTerm: string) => {
     new SearchVM({
       suggestionsEnabled: false,
       view: view,
+      autoSelect: false,
       sources: [
         {
           url: "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer",
           singleLineFieldName: "SingleLine",
           outFields: ["Addr_type"],
-          placeholder: "Adresse",
         },
       ],
     }),
@@ -29,21 +29,18 @@ export const useSearchViewModel = (searchTerm: string) => {
     queryKey: ["searchPlaces", searchTerm],
     queryFn: async () => {
       const searchResponse = await searchVM.current.search(searchTerm);
-      console.log("🚀 ~ queryFn: ~ searchResponse:", searchResponse);
       if (searchResponse.errors.length > 0) {
         throw new Error(searchResponse.errors[0].message);
       }
       if (searchResponse.results.length === 0) {
         throw new Error("No results found");
       }
-      return searchResponse.results[0];
+      return searchResponse.results[0].results.map((result) => ({
+        label: result.name,
+        value: (result.feature as any).uid, // TODO: Fix this
+        description: "",
+      }));
     },
     enabled: searchTerm.length > 0,
-    select: (data) =>
-      data.results.map((result) => ({
-        label: result.name,
-        value: result.feature.getObjectId().toString(),
-        description: "",
-      })),
   });
 };
