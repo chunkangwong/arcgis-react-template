24639b8b4639d4d845d02afea5d40ab3bdd3cc6b
ckwong <ckwong@esrisingapore.com.sg>
2024-03-12 22:29:02 +0800

    Handle ArrowUp and ArrowDown keydown event in SearchList

--- src/components/SearchDialog/SearchList.tsx
+++ src/components/SearchDialog/SearchList.tsx
@@ -21,25 +21,40 @@ export const SearchList = ({
     onSelect(value);
   };
 
+  const handleKeyDown = (value: string) => (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      onSelect(value);
+    } else if (e.key === "ArrowDown") {
+      e.currentTarget.nextSibling &&
+        (e.currentTarget.nextSibling as HTMLLIElement).focus();
+    } else if (e.key === "ArrowUp") {
+      e.currentTarget.previousSibling &&
+        (e.currentTarget.previousSibling as HTMLLIElement).focus();
+    }
+  };
+
   return (
     <ul className="max-h-[300px] overflow-y-auto overflow-x-hidden p-1">
-      {searchList.map((searchItem) => (
-        <li
-          className="group cursor-pointer flex items-center rounded-sm px-2 py-1.5 text-sm hover:bg-accent hover:text-accent-foreground"
-          key={searchItem.value}
-          onClick={handleSelect(searchItem.value)}
-        >
-          <Icon className="mr-2 h-4 w-4" />
-          <span className="group-hover:underline">{searchItem.label}</span>
-          {searchItem.description && (
-            <HelpHoverCard text={searchItem.description}>
-              <HelpCircle className="ml-auto h-4 w-4 opacity-0 group-hover:opacity-50 cursor-help" />
-            </HelpHoverCard>
-          )}
-        </li>
-      ))}
-      {searchList.length === 0 && (
+      {searchList.length === 0 ? (
         <p className="py-6 text-center text-sm">{emptyText}</p>
+      ) : (
+        searchList.map((searchItem) => (
+          <li
+            className="group cursor-pointer flex items-center rounded-sm px-2 py-1.5 text-sm hover:bg-accent hover:text-accent-foreground"
+            tabIndex={0}
+            key={searchItem.value}
+            onClick={handleSelect(searchItem.value)}
+            onKeyDown={handleKeyDown(searchItem.value)}
+          >
+            <Icon className="mr-2 h-4 w-4" />
+            <span className="group-hover:underline">{searchItem.label}</span>
+            {searchItem.description && (
+              <HelpHoverCard text={searchItem.description}>
+                <HelpCircle className="ml-auto h-4 w-4 opacity-0 group-hover:opacity-50 cursor-help" />
+              </HelpHoverCard>
+            )}
+          </li>
+        ))
       )}
       {moreButton}
     </ul>
