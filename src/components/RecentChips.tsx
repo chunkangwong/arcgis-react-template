aade03e1c6791e66373d5703d7d1cfcf44b5c3f7
ckwong <ckwong@esrisingapore.com.sg>
2024-03-20 22:04:16 +0800

    Added scrolling buttons

--- src/components/RecentChips.tsx
+++ src/components/RecentChips.tsx
@@ -1,6 +1,13 @@
-import { Component, Layers, Pin } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  Component,
+  Layers,
+  Pin,
+} from "lucide-react";
 
 import { useRecentStore } from "@/store/useRecentStore";
+import { useEffect, useRef, useState } from "react";
 import { Tooltip } from "./Tooltip";
 import { Button } from "./ui/button";
 
@@ -11,20 +18,85 @@ const chipIcons = {
 };
 
 export const RecentChips = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [scrollPosition, setScrollPosition] = useState<
+    "start" | "end" | "middle"
+  >("start");
+
   const recentItems = useRecentStore((state) => state.recentItems);
 
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    const handleScroll = () => {
+      if (container.scrollLeft === 0) {
+        setScrollPosition("start");
+      } else if (
+        container.scrollLeft + container.clientWidth >=
+        container.scrollWidth
+      ) {
+        setScrollPosition("end");
+      } else {
+        setScrollPosition("middle");
+      }
+    };
+
+    container.addEventListener("scroll", handleScroll);
+
+    return () => {
+      container.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = (direction: "left" | "right") => () => {
+    const container = containerRef.current;
+    if (!container) return;
+    if (direction === "left") {
+      container.scrollBy({
+        left: -container.clientWidth,
+        behavior: "smooth",
+      });
+    } else {
+      container.scrollBy({
+        left: container.clientWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <div className="flex gap-2">
-      {recentItems.map((item) => (
-        <Tooltip title={item.title} key={item.id} side="bottom">
-          <Button size="sm" className="flex rounded-3xl gap-x-2 px-2 h-8">
-            {chipIcons[item.type]}
-            <span className="text-xs max-w-32 overflow-hidden whitespace-nowrap text-ellipsis">
-              {item.title}
-            </span>
-          </Button>
-        </Tooltip>
-      ))}
+    <div className="flex w-full items-center">
+      <Button
+        disabled={scrollPosition === "start"}
+        size="icon"
+        variant="ghost"
+        className="h-8 w-8 disabled:opacity-20 transition-opacity"
+        onClick={handleScroll("left")}
+      >
+        <ChevronLeft />
+      </Button>
+      <div ref={containerRef} className="flex gap-2 w-full overflow-auto">
+        {recentItems.map((item) => (
+          <Tooltip title={item.title} key={item.id} side="bottom">
+            <Button size="sm" className="flex rounded-3xl gap-x-2 px-2 h-8">
+              {chipIcons[item.type]}
+              <span className="text-xs max-w-32 overflow-hidden whitespace-nowrap text-ellipsis">
+                {item.title}
+              </span>
+            </Button>
+          </Tooltip>
+        ))}
+      </div>
+      <Button
+        disabled={scrollPosition === "end"}
+        size="icon"
+        variant="ghost"
+        className="h-8 w-8 disabled:opacity-20 transition-opacity"
+        onClick={handleScroll("right")}
+      >
+        <ChevronRight />
+      </Button>
     </div>
   );
 };
